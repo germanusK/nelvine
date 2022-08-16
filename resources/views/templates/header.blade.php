cb5fd2fbab98815f73866de96b0dabe5ba4e3519
germanusK <germanuskeming@gmail.com>
2022-08-16 04:36:29 -0700

    project setup 2

---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,36 @@
+<div class="w-100">
+    <div class="w-100 bg-white d-flex justify-content-end py-2 flex-wrap">
+        <h5 class="fw-bold text-dark px-2">Text or Call : 678324097 (<span class="fa fa-phone text-secondary" ></span> NELVINE) </h5> |
+        <h5 class="fw-bold px-2">
+             <span class="fab fa-facebook mx-2"> </span>
+             <span class="fab fa-instagram mx-2"> </span>
+             <span class="fab fa-linkedin mx-2"> </span>
+             <span class="fab fa-twitter mx-2"> </span>
+             <span class="fab fa-youtube mx-2"> </span>
+             <span class=" fa-brands:tiktok"></span>
+             
+        </h5>
+    </div>
+    <div class="w-100 bg1">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a href="#" class="nav-link active">Active</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Action</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Another link</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link disabled">Disabled</a>
+            </li>
+        </ul>
+    </div>
+</div>
