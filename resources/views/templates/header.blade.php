8c9fecd782023751bbceba3ad78fc2ef8b710c79
germanusK <germanuskeming@gmail.com>
2022-08-19 13:02:23 -0700

    first major template uplaod

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,4 +1,5 @@
 <div class="w-100">
+    <h3 class="color-gold ">section 1:</h3>
     <div class="w-100 bg-white d-flex justify-content-end py-2 flex-wrap">
         <h6 class="fw-bolder text-dark px-2">Text or Call : 678324097 (<span class="fa fa-phone text-secondary" ></span> NELVINE) </h6> |
         <h5 class="fw-bold px-2">
@@ -13,7 +14,8 @@
         </h5>
     </div>
     <div class="w-100 bg1" >
-        <nav class="navbar navbar-expand-sm navbar-dark justify-content-between" >
+        <h3 class="color-gold ">section 2:</h3>
+        <nav class="navbar navbar-expand-sm justify-content-between" >
             <div class="col-md-3 text-center">
                 <a class="navbar-brand fs-2" href="#">NELVINE</a>
             </div>
@@ -42,6 +44,7 @@
             </div>
         </nav>
 
+        <h3 class="color-gold ">section 3:</h3>
         <div class="w-100 vh-100 d-flex flex-column justify-content-center text-white">
             <div class="w-100 h-25 fw-bolder text-white text-center">
                 <h2 class="header_alt_text1"></h2>
@@ -67,7 +70,7 @@
                             young man will help you make a <br>
                             Quantum Leap in your business.
                         </i></q>
-                        <h6 class="fw-bold text-dark text-center w-100 pt-3">DAN PEÑA</h6>
+                        <h6 class="fw-bold color-gold text-center w-100 pt-3">DAN PEÑA</h6>
                         <small class="text-center d-block">
                             The Trillion Dollar Man And <br>
                             Founder / Chairman Of The Guthrie Group
@@ -81,7 +84,7 @@
                             you and helping you how to make more money
                             faster than you ever dreamed possible.
                         </i></q>
-                        <h6 class="fw-bold text-dark text-center w-100 pt-3">BRIAN TRACY</h6>
+                        <h6 class="fw-bold color-gold text-center w-100 pt-3">BRIAN TRACY</h6>
                         <small class="text-center d-block">
                             World’s Premier Success Expert And <br>
                             Best-Selling Author Of Over 80 Books
@@ -92,18 +95,20 @@
             </div>
         </div>
 
-        <div class="w-100 py-3 d-flex flex-column justify-content-end py-5 bg2">
+        <h3 class="color-gold ">section 4:</h3>
+        <div class="w-100 py-3 d-flex flex-column justify-content-end py-3 bg2">
             <div class="container d-flex flex-wrap  justify-content-center">
                 @for($i = 0; $i < 10; $i++)
-                <div class="mx-auto  my-4 col-sm-2 col-md-1 fw-bolder fs-4 text-white border-left border-right border-light birder-2">
+                <div class="mx-auto  my-4 col-sm-2 col-md-1 fw-bolder fs-4 color-gold border-left border-right border-light birder-2">
                     <i class="fas fa-chess-bishop    "></i> abc
                 </div>
                 @endfor
             </div>
         </div>
 
-        <div class="d-flex flex-column justify-content-end w-100 bg3 py-5 bg-black">
-            <div class="row w-75 mx-auto text-white my-5">
+        <h3 class="color-gold ">section 5:</h3>
+        <div class="d-flex flex-column justify-content-end w-100 bg3 py-3">
+            <div class="row w-75 mx-auto color-gold my-5">
                 @for($i = 0; $i < 6; $i++)
                 <div class="col-sm-6 col-md-4 col-lg-2 border border-secondary border-top-0 border-right-0">
                     <div class="py-4 text-center">
@@ -116,79 +121,86 @@
         </div>
 
     </div>
+    <h3 class="color-gold ">section 6:</h3>
     <div class="w-100 d-flex flex-column justify-content-center bg4">
-        <div class="w-75 mx-auto text-center text-uppercase fs-1 fw-bolder text-white"> join our global <span style="color:coral"> community</span></div>
+        <div class="w-75 mx-auto text-center text-uppercase fs-1 fw-bolder color-black"> join our global <span class="color-gold"> community</span></div>
     </div>
     
+    <h3 class="color-gold ">section 7:</h3>
     <div class="w-100 d-flex flex-column justify-content-center bg5">
-        <div class="w-75 mx-auto text-center text-uppercase fs-1 fw-bolder text-white py-5">
+        <div class="w-75 mx-auto text-center text-uppercase fs-1 fw-bolder color-gold py-5">
             What's the #1 <span style="color:coral"> most important thing</span> <br>
             YOU WANT TO ACHIEVE RIGHT NOW?
         </div>
-        <div class="w-75 mx-auto text-center fs-3 fw-bolder text-light text-capitalize py-5">
+        <div class="w-75 mx-auto text-center fs-3 fw-bolder color-gold text-capitalize py-5">
             Select A Goal Below To Get Dan Lok’s World-Class Training <br>
             Solutions to Grow Your Income, Influence and Wealth Today
         </div>
     </div>
 
+    <h3 class="color-gold ">section 8:</h3>
     <div class="w-100 bg6 row">
         @for($i = 0; $i < 5; $i++)
             <div class="col-md d-flex flex-column justify-content-center bg6-1 border">
                 <div class="w-100 text-center">
-                    <div class="text-center fs-1 icon text-white my-2 hdn"><i class="fas fa-grin-alt    "></i></div>
-                    <div class="fs-3 fw-bolder text-white text-uppercase my-2">INCREASE YOUR INCOME</div>
+                    <div class="text-center fs-1 icon text-warning my-2 hdn"><i class="fas fa-grin-alt    "></i></div>
+                    <div class="fs-3 fw-bolder color-gold text-uppercase my-2">INCREASE YOUR INCOME</div>
                     <div class="fs-5 fw-bolder text-secondary text-capitalize my-2">Create more oppotunities</div>
-                    <div class="text-center fw-bolder text-danger text-uppercase hdn"><small >SAVE 50%</small></div>
+                    <div class="text-center fw-bolder color-gold text-uppercase hdn"><small >SAVE 50%</small></div>
                     <div class="fs-5 fw-bold text-light text-uppercase my-2 hdn">On training programs to increase my income</div>
                     <div class="w-75 mx-auto my-4 d-flex flex-column justify-content-center">
                         <hr class="border my-1">
                         <hr class="border my-1">
                     </div>
-                    <div class="text-center fw-bolder text-danger text-uppercase border-bottom border-3 border-danger py-2 w-75 mx-auto">Discover <i class="fas fa-plus-circle    "></i></div>
+                    <div class="text-center fw-bolder color-gold text-uppercase border-bottom border-3 border-danger py-2 w-75 mx-auto"><a href="" class=" color-gold"> Discover <i class="fas fa-plus-circle"></i></a></div>
                 </div>
             </div>
         @endfor
     </div>
+
     <div class="bg7 py-4 bg-white w-100">
-        <div class="container text-center text-black fs-1 fw-bolder text-uppercase">
+        <h3 class="color-gold ">section 9:</h3>
+        <div class="container text-center color-gold fs-1 fw-bolder text-uppercase pt-5">
             JOIN NELVINE IN SUPPORTING BE THE MATCH
         </div>
-        <div class="col-sm-7 col-md-4 mx-auto">
-            <img src="{{url('/images/image1.png')}}" alt="" class="w-100 h-auto">
+        <div class="col-sm-7 col-md-4 mx-auto text-uppercase fs-1 fw-bold text-capitalize text-warning py-5">
+           <i> Be <i class="fas fa-star-of-david color-gold"></i> the match </i>
         </div>
         <div class="container d-flex flex-wrap mx-auto">
             <div class="col-md-6">
                 <img src="{{url('/images/skuffwdd19.jpg')}}" alt="" class="w-100 h-100">
             </div>
             <div class="col-md-6 d-flex flex-column justify-content-center py-4 fs-5">
-                <div class="containermx-auto text-center">
+                <div class="containermx-auto text-center color-black">
                     <q>My mom is a cancer survivor. I lost my mother-in-law to cancer. I strongly believe in Be The Match’s mission to save people from life-threatening blood cancers such as leukaemia and lymphoma. I’d like to see a world where cellular therapies are readily available to everyone, regardless of race. Be the Match has my full support, and I’m proud to partner with them in the hope we can save as many people as humanly possible</q>
                 </div>
             </div>
         </div>
         <div class="container d-flex justify-content-center my-5">
-            <a href="" class="btn btn-danger px-5 py-3 border border-danger bg-white text-danger text-uppercase">join the registry <i class="fas fa-caret-right ml-4"></i></a>
+            <a href="" class="btn px-5 py-3 border border-danger bg-blk color-gold text-uppercase">join the registry <i class="fas fa-caret-right ml-4"></i></a>
         </div>
     </div>
 
     <div class="bg8 py-3 w-100 d-flex flex-column justify-content-center">
+        <h3 class="color-gold ">section 10:</h3>
         <div class="text-center text-uppercase col-md-75 col-lg-50 mx-auto text-light fs-1 fw-bolder">
-            upcoming <span style="color: rgb(250, 125, 103);">events</span>
+            upcoming <span class="color-gold">events</span>
         </div>
         <div class="container d-flex justify-content-center">
-            <a href="" class="btn py-1 px-3 text-uppercase fs-4 my-5 fw-bold text-white bg-danger">
+            <a href="" class="btn py-1 px-3 text-uppercase fs-4 my-5 fw-bold text-dark bg-warning">
                 view all events <i class="fas fa-caret-right fw-bolder  "></i>
             </a>
         </div>
     </div>
 
     <div class="container py-5 bg-white text-center bg9">
-        <div class="text-center text-primary text-uppercase my-2"><a href=""><small>Learn for free</small></a></div>
-        <div class="fs-1 fw-bolder text-dark text-center">
+        <h3 class="color-gold ">section 11:</h3>
+        <div class="text-center text-warning text-uppercase my-2"><a href="" class="text-warning"><small>Learn for free</small></a></div>
+        <div class="fs-1 fw-bolder color-black text-center">
             Insights To Elevate Your Financial Confidence <br>
              And Unlock Your Potential In Every Area Of LIfe
         </div>
-        <div class="fs-4 text-center text-dark mt-3 mb-5">From Dan Lok & Category Experts</div>
+        <div class="fs-4 text-center text-warning mt-3 mb-5">From Dan Lok & Category Experts</div>
         <div class="row py-1">
             @for($i=0; $i < 7; $i++)
             <div class="py-1 hder col-sm">
@@ -204,19 +216,19 @@
                     <div class="card">
                       <img class="card-img-top post-card-image w-full" src="{{asset('/images/skuffwdd19.jpg')}}" alt="Title">
                       <div class="card-img-overlay" style="background-color: rgba(0, 23, 60, 0.7);">
-                        <p class="card-text fs-2 text-uppercase fw-bolder text-center text-light">build up your <br> <span class="fs-1" style="color: rgb(250, 172, 158);"> ideal portfolio </span></p>
+                        <p class="card-text fs-2 text-uppercase fw-bolder text-center color-gold">build up your <br> <span class="fs-1" style="color: rgb(250, 172, 158);"> ideal portfolio </span></p>
                       </div>
                       <div class="card-body bg-light">
-                        <h6 class="card-title text-uppercase text-left text-success">HIGH INCOME SKILLS</h6>
+                        <h6 class="card-title text-uppercase text-left color-gold">HIGH INCOME SKILLS</h6>
                         <div class="card-text text-left text-secondary">
-                            <i><h4 class="text-uppercase my-2 ">
+                            <i><h4 class="text-uppercase my-2 color-black">
                                 7 Types of Investors: How to Invest And Build Up Your Ideal Portfolio
                             </h4></i>
                         </div>
-                        <small class="fw-bold">By <a href="" class="text-uppercase text-primary">team Nelvine</a></small>
-                        <div class="my-2 fs-5 text-left">Are you wondering how many types of investors are there? Every day, there are people…</div>
+                        <small class="fw-bold">By <a href="" class="text-uppercase color-gold">team Nelvine</a></small>
+                        <div class="my-2 fs-5 text-left color-gold">Are you wondering how many types of investors are there? Every day, there are people…</div>
                         <div class="d-flex justify-content-start">
-                            <a href="" class="text-danger text-uppercase"><small>read post <i class="fas fa-caret-right fw-bolder "></i></small></a>
+                            <a href="" class="color-black text-uppercase"><small>read post <i class="fas fa-caret-right fw-bolder "></i></small></a>
                         </div>
                       </div>
                     </div>
@@ -224,20 +236,22 @@
                 @endfor
             </div>
             <div class="d-flex justify-content-center my-5 py-2">
-                <a href="" class="btn btn-danger text-uppercase px-5 py-3">read the latest articles on our blog <i class="fas fa-caret-right fw-bolder  "></i></a>
+                <a href="" class="btn btn-warning text-uppercase text-decoration-none px-5 py-3 color-black">read the latest articles on our blog <i class="fas fa-caret-right fw-bolder  "></i></a>
             </div>
         </div>
     </div>
+
+    <h3 class="color-gold ">section 12:</h3>
     <div class="w-100 py-5 bg10 bg-dark">
-        <div class="my-5 py-5 text-center text-light">
+        <div class="my-5 py-5 text-center text-warning">
             <small>podcast</small>
-            <div class="fs-1 fw-bolder text-white">Conversations With The World’s Most <br>
+            <div class="fs-1 fw-bolder color-gold">Conversations With The World’s Most <br>
                 Successful & Influential Business Legends.</div>
                 <span>1, 037+ 
                     @for($i=0; $i < 5; $i++)
                     <i class="fas fa-star mx-1" style="color: tomato;"></i>
                     @endfor
-                    <span class="mx-2 text-danger fw-bold">|</span>
+                    <span class="mx-2 text-warning fw-bold">|</span>
                     477+ episodes
                 </span>
         </div>
@@ -245,19 +259,19 @@
             <div class="d-flex flex-row flex-nowrap">
                 @for($i=0; $i < 17; $i++)
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-                    <div class="card bg-transparent my-4 mx-1 rounded-lg border border-2 p-1">
+                    <div class="card bg-transparent my-4 mx-1 rounded-lg border border-2 border-secondary p-1">
                         <img src="{{asset('/images/skuffwdd23.jpg')}}" alt="casd image" class="card-img bg10-ci">
-                        <div class="card-img-overlay bg-image hover-zoom" style="background-color: rgba(0, 24, 58, 0.5);">
-                            <div class="text-light fs-3 fw-bold text-center text-uppercase">
+                        <div class="card-img-overlay bg-image hover-zoom" style="background-color: rgba(0, 12, 31, 0.7);">
+                            <div class="color-gold fs-3 fw-bold text-center text-uppercase">
                                 real estate lessons
                             </div>
                         </div>
-                        <h3 class="my-3 cart-title text-white text-center">Harmel rayat</h3>
-                        <p class="card-text text-light text-center">
+                        <h3 class="my-3 cart-title color-gold text-center">Harmel rayat</h3>
+                        <p class="card-text text-warning text-center">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus fuga dolores quis mollitia, maiores aut impedit nulla sed animi? Modi possimus sint laboriosam explicabo iste quos, dicta vero impedit obcaecati.
                         </p>
                         <div class="my-5 d-flex justify-content-center">
-                            <a href="" class="text-danger  text-uppercase">listen now <i class="fas fa-caret-right    "></i></a>
+                            <a href="" class="color-gold  text-uppercase">listen now <i class="fas fa-caret-right    "></i></a>
                         </div>
                     </div>
                 </div>
@@ -265,22 +279,23 @@
             </div>
         </div>
         <div class="d-flex justify-content-center">
-            <a href="" class="text-danger fs-1 px-4"><i class="fas fa-angle-left    "></i></a>
-            <a href="" class="text-danger fs-1 px-4"><i class="fas fa-angle-right    "></i></a>
+            <a href="" class="text-secondary fs-1 px-4"><i class="fas fa-angle-left    "></i></a>
+            <a href="" class="text-secondary fs-1 px-4"><i class="fas fa-angle-right    "></i></a>
         </div>
+        <h3 class="color-gold ">section 13:</h3>
     </div>
     
-    <div class="w-100 py-5 bg10 bg-dark">
+    <!-- <div class="w-100 py-5 bg10 bg-dark">
         <div class="my-5 py-5 text-center text-light">
             <small class="text-uppercase">watch now</small>
             <div class="fs-1 fw-bolder text-danger">NELFLIX</div>
         </div>
         <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
-            <!-- <ol class="carousel-indicators">
+            <ol class="carousel-indicators">
                 <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
                 <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
                 <li data-bs-target="#carouselId" data-bs-slide-to="2" aria-label="Third slide"></li>
-            </ol> -->
+            </ol>
             <div class="carousel-inner" role="listbox">
                 <div class="carousel-item active">
                     <img src="holder.js/900x500/auto/#777:#555/text:First slide" alt="First slide">
@@ -299,5 +314,5 @@
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
-    </div>
+    </div> -->
 </div>
